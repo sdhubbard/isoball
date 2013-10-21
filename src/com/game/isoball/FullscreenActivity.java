e735dd912dd7241b76949df0545c68c854a2e1ce
SergeantPsycho <SergeantPsycho@localhost>
2013-10-21 18:25:47 -0400

    - Work in progress for options buttons. - Temporarily disabled background music.

--- src/com/game/isoball/FullscreenActivity.java
+++ src/com/game/isoball/FullscreenActivity.java
@@ -2,8 +2,14 @@ package com.game.isoball;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.LinearLayout.LayoutParams;
+import android.widget.RelativeLayout;
 
 public class FullscreenActivity extends Activity {
     @Override
@@ -13,5 +19,16 @@ public class FullscreenActivity extends Activity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                             WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_fullscreen);
+        
+        FrameLayout frameLayout = (FrameLayout)findViewById(android.R.id.content);
+        LinearLayout view = (LinearLayout)getLayoutInflater().inflate(R.layout.options_layout, null);
+        LinearLayout.LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        
+        layoutParams.gravity = Gravity.BOTTOM;
+        view.setLayoutParams(layoutParams);
+        
+        frameLayout.addView(view);
+        view.setGravity(Gravity.BOTTOM);
+        
     }
 }
