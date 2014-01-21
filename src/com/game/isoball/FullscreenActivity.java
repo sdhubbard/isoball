95a0245fb8eafe7c86ba3657633207bd7ff66148
SergeantPsycho <SergeantPsycho@localhost>
2014-01-20 19:44:15 -0500

    -Integrated Nine Patch into app.

--- src/com/game/isoball/FullscreenActivity.java
+++ src/com/game/isoball/FullscreenActivity.java
@@ -6,12 +6,21 @@ import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.RelativeLayout;
+import android.widget.ToggleButton;
 
 public class FullscreenActivity extends Activity {
+	LinearLayout optionView;
+	ToggleButton optionsExpand;
+	ToggleButton optionsMusic;
+	ToggleButton optionsSound;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,14 +30,33 @@ public class FullscreenActivity extends Activity {
         setContentView(R.layout.activity_fullscreen);
         
         FrameLayout frameLayout = (FrameLayout)findViewById(android.R.id.content);
-        LinearLayout view = (LinearLayout)getLayoutInflater().inflate(R.layout.options_layout, null);
+        
+        optionView = (LinearLayout)getLayoutInflater().inflate(R.layout.options_layout, null);
         LinearLayout.LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
         
         layoutParams.gravity = Gravity.BOTTOM;
-        view.setLayoutParams(layoutParams);
+        optionView.setLayoutParams(layoutParams);
         
-        frameLayout.addView(view);
-        view.setGravity(Gravity.BOTTOM);
+        frameLayout.addView(optionView);
+        optionView.setGravity(Gravity.BOTTOM);
+        
+        optionsExpand = (ToggleButton)optionView.findViewById(R.id.tb_options_expand);
+        optionsMusic = (ToggleButton)optionView.findViewById(R.id.tb_options__music);
+        optionsSound = (ToggleButton)optionView.findViewById(R.id.tb_options_sound);
+        
+        optionsExpand.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if (isChecked) {
+					optionsMusic.setVisibility(View.VISIBLE);
+					optionsSound.setVisibility(View.VISIBLE);
+				} else {
+					optionsMusic.setVisibility(View.GONE);
+					optionsSound.setVisibility(View.GONE);
+				}
+			}
+		});
         
     }
 }
