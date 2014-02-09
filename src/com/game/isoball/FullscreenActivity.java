ac0ce23e8a5b61bef4d7bdc72fe3f779600bdf53
SergeantPsycho <SergeantPsycho@localhost>
2014-02-09 18:03:59 -0500

    Implemented saving of sound and music preferences.

--- src/com/game/isoball/FullscreenActivity.java
+++ src/com/game/isoball/FullscreenActivity.java
@@ -1,7 +1,10 @@
 package com.game.isoball;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Window;
@@ -16,10 +19,11 @@ import android.widget.RelativeLayout;
 import android.widget.ToggleButton;
 
 public class FullscreenActivity extends Activity {
-	LinearLayout optionView;
-	ToggleButton optionsExpand;
-	ToggleButton optionsMusic;
-	ToggleButton optionsSound;
+	private LinearLayout optionView;
+	private ToggleButton optionsExpand;
+	private ToggleButton optionsMusic;
+	private ToggleButton optionsSound;
+	private SharedPreferences sharedPreferences;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,6 +48,8 @@ public class FullscreenActivity extends Activity {
         optionsMusic = (ToggleButton)optionView.findViewById(R.id.tb_options__music);
         optionsSound = (ToggleButton)optionView.findViewById(R.id.tb_options_sound);
         
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+        
         optionsExpand.setOnCheckedChangeListener(new OnCheckedChangeListener() {
 			
 			@Override
@@ -58,5 +64,41 @@ public class FullscreenActivity extends Activity {
 			}
 		});
         
+        configureMusicOptions();
+        configureSoundOptions();
+    }
+    
+    private void configureMusicOptions() {
+    	boolean musicOn = true;
+    	final String musicSettingKey = getString(R.string.music_setting_key);
+    	
+    	musicOn = sharedPreferences.getBoolean(musicSettingKey, true);
+    	optionsMusic.setChecked(musicOn);
+    	
+    	optionsMusic.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				sharedPreferences.edit().putBoolean(musicSettingKey, isChecked).commit();
+				
+			}
+		});
+    }
+    
+    private void configureSoundOptions() {
+    	boolean soundOn = true;
+    	final String soundSettingKey = getString(R.string.sound_setting_key);
+    	
+    	soundOn = sharedPreferences.getBoolean(soundSettingKey, true);
+    	optionsSound.setChecked(soundOn);
+    	
+    	optionsSound.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+			
+			@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				sharedPreferences.edit().putBoolean(soundSettingKey, isChecked).commit();
+				
+			}
+		});
     }
 }
